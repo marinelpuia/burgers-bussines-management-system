68b510e1a027206868a04019fd234763a5e62b9b
Marinel Puia <megatron1975@yahoo.co.uk>
2022-01-04 18:37:05 +0200

    Initial Commit

---
+++ src/main/java/ro/puiamarinel/mpbms/MpbmsApplication.java
@@ -0,0 +1,13 @@
+package ro.puiamarinel.mpbms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpbmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpbmsApplication.class, args);
+	}
+
+}
